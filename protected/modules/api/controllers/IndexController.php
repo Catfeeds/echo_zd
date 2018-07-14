08c8b9b393643218d41d3eed47273da03d2321a8
ztenst <754710007@qq.com>
2018-07-14 20:57:37 +0800

    yh

--- protected/modules/api/controllers/IndexController.php
+++ protected/modules/api/controllers/IndexController.php
@@ -589,14 +589,16 @@ class IndexController extends ApiController
                             'openid'=>$openid,
                             'session_key'=>$cont['session_key'],
                         ];
-                        // $this->frame['data'] = $data;
-                        echo json_encode($data);
+                        $this->frame['data'] = $data;
+                        // echo json_encode($data);
                     } else {
-                        // $this->frame['data'] = ['openid'=>$cont['openid'],'session_key'=>$cont['session_key'],'uid'=>''];
-                        echo json_encode(['openid'=>$cont['openid'],'session_key'=>$cont['session_key'],'uid'=>'']);
+                        $this->frame['data'] = ['openid'=>$cont['openid'],'session_key'=>$cont['session_key'],'uid'=>''];
+                        // echo json_encode(['openid'=>$cont['openid'],'session_key'=>$cont['session_key'],'uid'=>'']);
                     }
+                } else {
+                    $this->returnError($cont['errorMsg']);
                 }
-                Yii::app()->end();
+                // Yii::app()->end();
             }
                 
         }
