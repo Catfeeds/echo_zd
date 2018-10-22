d9ef09be577b98f4cc640181701f87961e8dc2a3
ztenst <754710007@qq.com>
2018-10-22 20:46:35 +0800

    th

--- protected/modules/api/controllers/UserController.php
+++ protected/modules/api/controllers/UserController.php
@@ -1094,7 +1094,7 @@ class UserController extends ApiController{
     	
 	}
 
-	public function actionAnIndex($uid='',$user_type='',$day='1')
+	public function actionAnIndex($w='',$user_type='',$day='1')
 	{
 		$user = StaffExt::model()->findByPk($uid);
 		$data = $tags = [];
@@ -1280,9 +1280,9 @@ class UserController extends ApiController{
         // var_dump(count($subs));exit;
     	if($subs) {
     		foreach ($subs as $s) {
-    			// if($s->created>$tobe) {
+    			if($s->created>$tobe) {
     				$todarr['报备'] += 1;
-    			// }
+    			}
     			if ($s->status==1) {
     				$todarr['到访'] += 1;
     			} elseif ($s->status==3) {
