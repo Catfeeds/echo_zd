ce3942f4d7843f13ee67103fe030c234f3f04219
ztenst <754710007@qq.com>
2018-11-19 16:52:36 +0800

    yh\

--- protected/models_ext/SubExt.php
+++ protected/models_ext/SubExt.php
@@ -144,7 +144,7 @@ class SubExt extends Sub{
         if($this->getIsNewRecord()) {
             // 如果有help_uid 说明辅助报备 则发短信给分销
             if($user = $this->user)
-                SmsExt::sendMsg('发送客户码链接',$this->fx_phone,['name'=>$user->name,'khm'=>$this->getShort($this->id).",",'kh'=>$this->name.$this->phone]);
+                SmsExt::sendMsg('发送客户码链接',$this->fx_phone,['name'=>$user->name,'khm'=>$this->getShort($this->id)."。",'kh'=>$this->name.$this->phone]);
         }
         parent::afterSave();
     }
